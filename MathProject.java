4aebec7d8e3d3666b072e4d5d0969c3d4006255a
wiltshi6 <mellon@Gregorys-MacBook-Pro.local>
2017-07-12 16:04:56 -0400

    initial commit

---
+++ MathProject.java
@@ -0,0 +1,16 @@
+package mathprojectabc;
+
+import javax.swing.*;
+
+/**
+ * Created by mellon on 7/11/17.
+ */
+public class MathProject {
+
+    public static void main(String[] args) {
+        GuiFrame frame = new GuiFrame();
+        frame.setTitle("Math Project");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
